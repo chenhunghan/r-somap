0101a738916d356f2708ae233fb487844ca62991
Hung-Han Chen <chenhungh@gmail.com>
2017-04-05 18:29:20 +0300

    Add eslint ignore

--- src/euclideanDistance.test.js
+++ src/euclideanDistance.test.js
@@ -8,6 +8,6 @@ test('return correct euclidean distance of two three-dimensional points', () => 
 	expect(euclideanDistance([0, 0, 3], [4, 0, 0])).toBe(5)
 ));
 
-test('throws errors when dimension did not match', () => {
-  expect(() => euclideanDistance([0, 0], [0, 0, 0])).toThrowError();
+test('throws errors when dimension did not match', () => { // eslint-disable-line fp/no-nil
+  expect(() => euclideanDistance([0, 0], [0, 0, 0])).toThrowError('euclidean distance algorithm: two vectors in differnt dimension. vector 1 in 2; vector 2 = 3');
 });
