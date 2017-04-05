a4853ee297b61515bb37a197727df3e800bafa6c
Hung-Han Chen <chenhungh@gmail.com>
2017-04-05 18:29:54 +0300

    Change the var name from point to vector

--- src/euclideanDistance.js
+++ src/euclideanDistance.js
@@ -1,14 +1,14 @@
 // @flow
-export const euclideanDistance = (point1: Array<number>, point2: Array<number>): number => {
-  if (point1.length !== point2.length) {
-    throw new Error(`euclidean distance algorithm: two input points in differnt dimension. point 1 in ${point1.length}; point 2 has ${point2.length}`);
+export const euclideanDistance = (vector1: Array<number>, vector2: Array<number>): number => {
+  if (vector1.length !== vector2.length) {
+    throw new Error(`euclidean distance algorithm: two vectors in differnt dimension. vector 1 in ${vector1.length}; vector 2 = ${vector2.length}`);
   }
-  return Math.hypot(...point1.map((point1Value, index) => point1Value - point2[index]));
+  return Math.hypot(...vector1.map((vector1Value, index) => vector1Value - vector2[index]));
   // above is the same as:
   // let sum = 0;
-  // const length = point1.length; // or point2.length since they have the same dimension
+  // const length = vector1.length; // or vector2.length since they have the same dimension
   // for (let index = 0; index < length; index += 1) {
-  //   sum += Math.pow(point1[index] - point2[index], 2);
+  //   sum += Math.pow(vector1[index] - vector2[index], 2);
   // }
   // return Math.sqrt(sum);
 };
