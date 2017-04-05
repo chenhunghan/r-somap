c9e6ec9620d61ebf1073031f0a1dc935491d5481
Hung-Han Chen <chenhungh@gmail.com>
2017-04-06 00:13:14 +0300

    Edit test description and add an new test for times > 1

--- src/getFinalMap.test.js
+++ src/getFinalMap.test.js
@@ -14,19 +14,18 @@ const SOMap = [
   [101, 3, 10], [0, 3, 8], [1000, 9999, 9998],
 ];
 const widthOfSOMap = 3;
-const timesPerInputSample = 1;
 const baseDenominator = 1;
 const baseLearningRadius = 2;
 const baseLearningRate = 0.5;
 const au0 = mapUnit => updater(inputDataSet[0], mapUnit, baseLearningRate);
 const au1 = mapUnit => updater(inputDataSet[1], mapUnit, baseLearningRate);
 
-test('updateBMU should update BMU on given SOMap according to sampleVector', () => (
+test('getFinalMap should get map with 1+ input vector sample(s)', () => (
   expect(getFinalMap(
     inputDataSet,
     SOMap,
     widthOfSOMap,
-    timesPerInputSample,
+    1,
     baseDenominator,
     baseLearningRadius,
     baseLearningRate,
@@ -37,5 +36,29 @@ test('updateBMU should update BMU on given SOMap according to sampleVector', () 
         [100, 3, 10], 		         [0, 2, 8],	 		  au0([0, 2, 1]),
         [100, 3, 10], 	       au0([0, 2, 2]),	 		au0([0, 1, 1]),
     au0([101, 3, 10]),	 		   au0([0, 3, 8]),	 		au0([1000, 9999, 9998]),
+  ])
+));
+
+const denominator = (1 + 1 / baseDenominator);
+const learningRateAtSecondCycle = (baseLearningRate / denominator);
+const au0t = mapUnit => updater(inputDataSet[0], mapUnit, learningRateAtSecondCycle);
+const au1t = mapUnit => updater(inputDataSet[1], mapUnit, learningRateAtSecondCycle);
+
+test('getFinalMap should get updated map for timesPerInputSample 1+', () => (
+  expect(getFinalMap(
+    inputDataSet,
+    SOMap,
+    widthOfSOMap,
+    2,
+    baseDenominator,
+    baseLearningRadius,
+    baseLearningRate,
+  )).toEqual([
+    au1t(au1([-1.2, -1.2, -1.2])), 	 au1t(au1([0, 2, 8])),	 		au1([0, 2, 1]),
+    au1t(au1([100, 3, 10])), 		          au1([0, 2, 2]),	 		      [0, 1, 1],
+    au1([101, 3, 10]), 		                    [0, 3, 8],	 	        [0, 0, 1],
+        [100, 3, 10], 		                    [0, 2, 8],	 		  au0([0, 2, 1]),
+        [100, 3, 10], 	                  au0([0, 2, 2]),	 au0t(au0([0, 1, 1])),
+    au0([101, 3, 10]),	 		        au0t(au0([0, 3, 8])),	 au0t(au0([1000, 9999, 9998])),
   ])
 ));
