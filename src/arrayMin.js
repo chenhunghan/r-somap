53776e38800d81c5e70305b1e8ab5da8716c8e2f
Hung-Han Chen <chenhungh@gmail.com>
2017-04-05 18:32:09 +0300

    Fit the rules of functional programing, use array.reduce instead of for-loop

--- src/arrayMin.js
+++ src/arrayMin.js
@@ -1,10 +1,13 @@
 // @flow
 export const arrayMinIndex = (arr: Array<number>): number => {
-  let lowest = 0;
-  for (let i = 1; i < arr.length; i += 1) {
-    if (arr[i] < arr[lowest]) {
-      lowest = i;
-    }
-  }
-  return lowest;
+  const lowest = arr.reduce((acc, v) => Math.min(acc, v));
+  return arr.indexOf(lowest);
+  // same as
+  // let lowest = 0;
+  // for (let i = 1; i < arr.length; i += 1) {
+  //   if (arr[i] < arr[lowest]) {
+  //     lowest = i;
+  //   }
+  // }
+  // return lowest;
 };
