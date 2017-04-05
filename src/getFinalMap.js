5f062a8a7e33105d9579374ca4e70a2ff50d191e
Hung-Han Chen <chenhungh@gmail.com>
2017-04-06 00:11:39 +0300

    Edit to make it functional reactive

--- src/getFinalMap.js
+++ src/getFinalMap.js
@@ -1,31 +1,48 @@
 // @flow
 import { learn } from './learn';
-import { initMap } from './initMap';
+import { createMap } from './createMap';
 
 export const getFinalMap = (
   inputDataSet: Array<Array<number>>,
-  SOMap: Array<Array<number>> = initMap(8, 12, 3),
+  initMap: Array<Array<number>> = createMap(8, 12, 3),
   widthOfSOMap: number = 8,
   timesPerInputSample: number = 100,
   baseDenominator: number = 300,
   baseLearningRadius: number = 3,
   baseLearningRate: number = 0.3,
 ): Array<Array<number>> => {
-  const times = Array.from(new Array(timesPerInputSample), (x, i) => i);
-  let trainedMap = SOMap;
-  inputDataSet.forEach((sampleVector) => {
-    times.forEach((t) => {
-      const denominator = (1 + t / baseDenominator);
-      const learningRadius = (baseLearningRadius / denominator);
-      const learningRate = (baseLearningRate / denominator);
-      trainedMap = learn(
-        trainedMap,
+  const times = Array.from({ length: timesPerInputSample }, (v, i) => i);
+  return times.reduce(((accumulatingMapWithTime, t) => {
+    const denominator = (1 + t / baseDenominator);
+    const learningRadius = (baseLearningRadius / denominator);
+    const learningRate = (baseLearningRate / denominator);
+    return inputDataSet.reduce((accumulatingMap, sampleVector) => {
+      const newMap = learn(
+        accumulatingMap,
         widthOfSOMap,
         sampleVector,
         learningRate,
         learningRadius,
       ).SOMap;
-    });
-  });
-  return trainedMap;
-};
+      return newMap;
+    }, accumulatingMapWithTime);
+  }), initMap);
+  // above is the functional reactive version of:
+  //
+  // let trainedMap = SOMap;
+  // inputDataSet.forEach((sampleVector) => {
+  //   times.forEach((t) => {
+  //     const denominator = (1 + t / baseDenominator);
+  //     const learningRadius = (baseLearningRadius / denominator);
+  //     const learningRate = (baseLearningRate / denominator);
+  //     trainedMap = learn(
+  //       trainedMap,
+  //       widthOfSOMap,
+  //       sampleVector,
+  //       learningRate,
+  //       learningRadius,
+  //     ).SOMap;
+  //   });
+  // });
+  // return trainedMap;
+}
