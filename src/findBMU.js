94bd6f40623a5c600fa2a93e87153b8d4cdc23ac
Hung-Han Chen <chenhungh@gmail.com>
2017-04-06 17:19:40 +0300

    Add new mode to place map units on a hex map of hexagonal lattices

--- src/findBMU.js
+++ src/findBMU.js
@@ -2,7 +2,7 @@
 import { arrayMinIndex } from './arrayMin';
 import { euclideanDistance } from './euclideanDistance';
 
-export const findBMU = (
+export const findBMUinRect = (
   SOMap: Array<Array<number>>,
   widthOfSOMap: number,
   sampleVector: Array<number>,
@@ -24,15 +24,47 @@ export const findBMU = (
   };
 };
 
-export const simpleFindBMU = (
+export const findBMUinHex = (
   SOMap: Array<Array<number>>,
   widthOfSOMap: number,
   sampleVector: Array<number>,
-): Array<number> => {
+  padding: number = 1,
+): {
+  index: number,
+  coordination: Array<number>,
+  vector: Array<number>,
+} => {
   const distanceCollection = SOMap.map(mapUnit => euclideanDistance(mapUnit, sampleVector));
   const index = arrayMinIndex(distanceCollection); // Winner unit's index, should be an intergar.
-  return [
-    index % widthOfSOMap,
-    Math.floor(index / widthOfSOMap),
-  ];
+  const rowN = Math.floor(index / widthOfSOMap);
+  const d = Math.sqrt(padding * padding / 2);
+  if (rowN % 2 === 0) {
+    return {
+      index,
+      coordination: [
+        index % widthOfSOMap * padding,
+        rowN * d,
+      ],
+      vector: SOMap[index],
+    };
+  }
+  return {
+    index,
+    coordination: [
+      index % widthOfSOMap + d,
+      rowN * d,
+    ],
+    vector: SOMap[index],
+  };
 };
+
+export const findBMU = (
+  SOMap: Array<Array<number>>,
+  widthOfSOMap: number,
+  sampleVector: Array<number>,
+  padding: number = 1,
+): {
+  index: number,
+  coordination: Array<number>,
+  vector: Array<number>,
+} => findBMUinHex(SOMap, widthOfSOMap, sampleVector, padding);
--- src/getCoordiOnMap.js
+++ src/getCoordiOnMap.js
@@ -1,6 +1,6 @@
 // @flow
 
-export const getCoordiOnMap = (
+export const getXYinRect = (
   array: Array<Array<number>>,
   widthOfSOMap: number,
 ): Array<{
@@ -24,3 +24,49 @@ export const getCoordiOnMap = (
   });
   return arrayWithScaledDimension;
 };
+
+export const getXYinHex = (
+  array: Array<Array<number>>,
+  widthOfSOMap: number,
+  padding: number = 1,
+): Array<{
+  index: number,
+  vector: Array<number>,
+  coordination: Array<number>,
+}> => {
+  if (!Number.isInteger(widthOfSOMap)) {
+    throw new Error('coordination scaler: typeof widthOfSOMap should be integer');
+  }
+  const arrayWithScaledDimension = array.map((vector, index) => {
+    const rowN = Math.floor(index / widthOfSOMap);
+    const d = Math.sqrt(padding * padding / 2);
+    if (rowN % 2 === 0) {
+      return {
+        index,
+        vector,
+        coordination: [
+          index % widthOfSOMap * padding,
+          rowN * d,
+        ],
+      };
+    }
+    return {
+      index,
+      vector,
+      coordination: [
+        index % widthOfSOMap + d,
+        rowN * d,
+      ],
+    };
+  });
+  return arrayWithScaledDimension;
+};
+
+export const getCoordiOnMap = (
+  array: Array<Array<number>>,
+  widthOfSOMap: number,
+): Array<{
+  index: number,
+  vector: Array<number>,
+  coordination: Array<number>,
+}> => getXYinHex(array, widthOfSOMap);
