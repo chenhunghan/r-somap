25015a265daf9e79823b558735134ed65358f8ca
Hung-Han Chen <chenhungh@gmail.com>
2017-04-09 18:41:50 +0300

    Add Katex injection

--- src/stepwiseRecursiveSOM.js
+++ src/stepwiseRecursiveSOM.js
@@ -24,6 +24,15 @@ The step wise recursive SOM will reiterated the modifications for given epochs.
 \[ \sum_t{m_{i}(t+1)} = \sum_t{m_{i}(t)} + \sum_t{h_{ci}(t)[x(t) - m_{i}(t)]} \]
 </span>
 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.7.1/katex.min.css" integrity="sha384-wITovz90syo1dJWVh32uuETPVEtGigN07tkttEqPv+uR2SE/mbQcG7ATL28aI9H0" crossorigin="anonymous">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.7.1/katex.min.js" integrity="sha384-/y1Nn9+QQAipbNQWU65krzJralCnuOasHncUFXGkdwntGeSvQicrYkiUBwsgUqc1" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.7.1/contrib/auto-render.min.js" integrity="sha384-dq1/gEHSxPZQ7DdrM82ID4YVol9BYyU7GbWlIwnwyPzotpoc57wDw/guX8EaYGPx" crossorigin="anonymous">
+</script>
+<script>
+  document.addEventListener("DOMContentLoaded", function() {
+    renderMathInElement(document.body);
+  });
+</script>
 
 * @param inputDataSet - the input data set.
 * @param mapSize - the size of the map, i.e., the gird. Default = 5 * Math.sqrt(inputDataSet.length) as suggested by SOM Toolbox; Kohonen (2012) claims "It is not possible to guess or estimate the exact size of the array beforehand. It must be determined by the trial-and-error method, after seeing the quality of the first guess.
